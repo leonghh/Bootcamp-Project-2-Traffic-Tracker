14879e9fe8eeefebab859facbcb3b46f707eb76e
Vegeta <56867717+pozengineer@users.noreply.github.com>
2020-03-02 17:51:45 +1100

    fixed loop to retrieve lat and long from API

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var express = require("express");
+var express = require("express")
 
 var PORT = process.env.PORT || 8080;
 
@@ -9,13 +9,13 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 var routes = require("./controllers/router.js");
-
 app.use(routes);
 
 app.listen(PORT, function() {
