1f16e0b3986601f5f06de855081a5d20a627442c
Eugene Leong <honghao@hotmail.sg>
2020-03-07 12:45:04 +1100

    change route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -13,7 +13,7 @@ var db = require("../models");
 // =============================================================
 module.exports = function(app) {
 
-  app.get("/", function(req, res) {
+  app.get("*", function(req, res) {
     res.render("index");
   });
 
